8b84eb84d4e44ee1db6d7e0c0de905c850572492
vuong4-design <inctrancvuong4@gmail.com>
2026-02-02 02:17:48 +0700

    Send export transactions to backend endpoint

--- SharedLibraries/HttpUtil.m
+++ SharedLibraries/HttpUtil.m
@@ -1,5 +1,11 @@
 #import "HttpUtil.h"
 
+#ifndef API_HOST
+#define API_HOST ""
+#endif
+
+static NSString *const kExportEndpointPath = @"/api/inventory/export";
+
 @implementation HttpUtil
 
 + (instancetype)sharedInstance {
@@ -47,10 +53,61 @@ completedHandler:(void (^)(NSData *data, NSURLResponse *response, NSError *error
 
 - (void)exportItemToInventory:(NSDictionary *)params
                    completion:(void (^)(NSInteger code, id data))completion {
-	(void)params;
-	if (completion) {
-		completion(200, @{});
+	NSString *host = @API_HOST;
+	if (host.length == 0) {
+		NSLog(@"DEBUG* export endpoint missing API_HOST");
+		if (completion) {
+			completion(0, nil);
+		}
+		return;
 	}
+
+	NSString *urlString = [NSString stringWithFormat:@"%@%@", host, kExportEndpointPath];
+	NSURL *url = [NSURL URLWithString:urlString];
+	if (!url) {
+		NSLog(@"DEBUG* export endpoint invalid URL: %@", urlString);
+		if (completion) {
+			completion(0, nil);
+		}
+		return;
+	}
+
+	NSError *error = nil;
+	NSData *body = [NSJSONSerialization dataWithJSONObject:params ?: @{} options:0 error:&error];
+	if (error) {
+		NSLog(@"DEBUG* export endpoint JSON error: %@", error.localizedDescription);
+		if (completion) {
+			completion(0, nil);
+		}
+		return;
+	}
+
+	NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+	request.HTTPMethod = @"POST";
+	[request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+	request.HTTPBody = body;
+
+	NSURLSessionDataTask *task = [[NSURLSession sharedSession]
+		dataTaskWithRequest:request
+		completionHandler:^(NSData *data, NSURLResponse *response, NSError *requestError) {
+			if (requestError) {
+				NSLog(@"DEBUG* export endpoint request error: %@", requestError.localizedDescription);
+				if (completion) {
+					completion(0, nil);
+				}
+				return;
+			}
+
+			NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+			id parsed = nil;
+			if (data.length > 0) {
+				parsed = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+			}
+			if (completion) {
+				completion(httpResponse.statusCode, parsed);
+			}
+		}];
+	[task resume];
 }
 
 - (void)checkInventoryForProduct:(NSString *)productID
